45edffdf3dca613a7039261ef30360268c81aefb
Pro <pro@Pros-MacBook-Pro.local>
2022-08-05 18:07:18 -0500

    code readability update

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const navbar = document.querySelector('nav');
 const htmlElement = document.querySelector('html');
 const topElement = document.querySelector('.top');
-const height = topElement.clientHeight;
-let currentOpacity = .4;    
+let currentOpacity; 
 window.addEventListener('scroll', () =>{
     
+    /* final currentOpacity will be from 0-1 incrementing by 
+    .1 depending on scroll position. */
     currentOpacity  = -topElement.clientHeight - htmlElement.scrollTop - 200;
     currentOpacity = currentOpacity / 100 * .25;
     currentOpacity = Math.abs(currentOpacity) - 1;
+    
+    
     if(currentOpacity < 1){
         disappearNav();
     }
-    
+     //limits opacity to no less than .4 and no more than 1
     if(currentOpacity <= .4){
         currentOpacity = .4;
-        console.log('here');
     }else if(currentOpacity >=1){
         currentOpacity = 1;
         changeNav();
     }
-    console.log(currentOpacity);
+    
     topElement.style.backgroundColor = `hsla(30, 100%, 50%,${currentOpacity})`;
     
 
